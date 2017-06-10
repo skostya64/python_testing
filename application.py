7b3f741fea7ba3d44ee182ec44706013a35deacd
Konstantin Styagailo <skostya64@gmail.com>
2017-06-10 13:28:28 +0300

    commit

---
+++ application.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+
+
+class Application:
+
+    def _init_(self):
+        self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        self.wd.implicitly_wait(60)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_add_new_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create_new(self, new):
+        wd = self.wd
+        self.open_home_page()
+        # init new creation
+        wd.find_element_by_name("firstname").click()
+        # fill new firm
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(new.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(new.middlename)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(new.address)
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, group):
+        wd = self.wd
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group firm
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_xpath("//div[@id='content']/form").click()
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def destroy(self):
+        self.wd.quit()
--- test_add_new.py
+++ test_add_new.py
@@ -1,70 +1,26 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from new import New
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_new(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
 
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+def test_add_new(app):
+    app.login(username="admin", password="secret")
+    app.create_new(New(firstname="Konstantin", middlename="Styagailo", address="Parkovaya, 8"))
+    app.logout()
 
-    def open_add_new_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
 
-    def create_new(self, wd, new):
-        # init new creation
-        wd.find_element_by_name("firstname").click()
-        # fill new firm
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(new.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(new.middlename)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(new.address)
+def test_empty_new(app):
+    app.login(username="admin", password="secret")
+    app.create_new(New(firstname="", middlename="", address=""))
+    app.logout()
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
 
-    def test_test_add_new(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_add_new_page(wd)
-        self.create_new(wd, New(firstname="Konstantin", middlename="Styagailo", address="Parkovaya, 8"))
-        self.logout(wd)
 
-    def test_test_empty_new(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_add_new_page(wd)
-        self.create_new(wd, New(firstname="", middlename="", address=""))
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
